856c9a3dcd2af768a4e35bdfb71068d4f2ef152f
Yuri²² <gabriellisara1301@gmail.com>
2024-11-29 16:23:58 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    '<i>Old School</i>',
+    '<i>Força</i>',
     'Quais são as principais vantagens de se ter uma caminhonete?',
     ' As caminhonetes oferecem maior capacidade de carga, versatilidade para diferentes tipos de terreno e robustez. Elas são ideais para quem precisa transportar objetos grandes ou trabalhar em áreas rurais ou de difícil acesso, além de proporcionar mais conforto em viagens longas!'
 )
 
 criaCartao(
-    '<i>Ursos de força</i>',
+    '<i>Disel</i>',
     'Caminhonetes consomem mais combustível que carros de passeio?',
     '   Sim, as caminhonetes geralmente consomem mais combustível que carros de passeio devido ao seu tamanho maior e peso mais elevado. No entanto, o consumo depende do modelo, tipo de motor e da condução. Modelos mais modernos e eficientes podem ter desempenho de combustível mais próximo ao de carros menores.'
 )
 
 criaCartao(
-    '<i>Aesthetic Inspiration</i>',
+    '<i>Off-Road</i>',
     'Quais são as melhores caminhonetes para uso off-road?',
     '  Modelos como a Toyota Hilux, Ford Ranger e Jeep Gladiator são frequentemente citados como alguns dos melhores para uso off-road. Eles são projetados para enfrentar terrenos difíceis, com tração nas quatro rodas e suspensão reforçada.'
 )
 
 criaCartao(
-    'Superação',
+    '<i>Famílias</i>',
     ' Caminhonetes são boas para o transporte de famílias?',
     ' Sim, muitas caminhonetes, especialmente os modelos maiores como a Chevrolet Silverado ou a Ram 1500, oferecem espaço suficiente para transportar famílias, com bancos traseiros confortáveis e recursos modernos de segurança e entretenimento, tornando-as opções viáveis para quem precisa de mais espaço e potência.'
 )
